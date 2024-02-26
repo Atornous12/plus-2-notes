107add578974cf3901ddecfc31aa29183fc25877
Atornous <105731423+Atornous12@users.noreply.github.com>
2024-02-26 18:14:13 +0545

    Add files via upload

---
+++ 12-page.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("Physics").onclick = function () {
+        window.location.href = "12-science/Physics.html";
+    };
+
+    document.getElementById("Chemistry").onclick = function () {
+        window.location.href = "12-science/Chemistry.html";
+    };
+    document.getElementById("Math").onclick = function () {
+        window.location.href = "12-science/Math.html";
+    };
+    document.getElementById("Nepali").onclick = function () {
+        window.location.href = "12-science/Nepali.html";
+    };
+    document.getElementById("English").onclick = function () {
+        window.location.href = "12-science/English.html";
+    };
+    document.getElementById("Computer").onclick = function () {
+        window.location.href = "12-science/Computer.html";
+    };
+    document.getElementById("Biology").onclick = function () {
+        window.location.href = "12-science/Biology.html";
+    };
+
+});
---
+++ main-page.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("class11").onclick = function () {
+        window.location.href = "11.html";
+    };
+
+    document.getElementById("class12").onclick = function () {
+        window.location.href = "12.html";
+    };
+});
